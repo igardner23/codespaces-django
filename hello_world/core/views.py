ebce8b709f641e5cd55f582a3e8dcd362addff4c
igardner23 <39102593+igardner23@users.noreply.github.com>
2023-03-21 14:08:38 +0000

    Cool, had to change some things, really I should've done a lot more before an initial commit, but here we are
    I don't know what I'm doing with version control ( If that wasn't clear ) But I am happy to learn.

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -1,30 +1,9 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from rest_framework import permissions
-from hello_world.core.serializers import UserSerializer, GroupSerializer
+
 from django.http import HttpResponse
 
 # Going to want to create a new viewset? Also has to handle post information too
 
 from django.http import HttpResponse
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
 
 
 def basic_endpoint(request):
